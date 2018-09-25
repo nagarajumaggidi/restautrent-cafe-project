b05251080339b6af61673ac5205f8f18eb0e10f5
nagarajumaggidi <nagarajumaggidi4@gmail.com>
2018-09-25 16:47:24 +0530

    Hello

---
+++ restaurant&cafe/assests/bootstrap/js/validation.js
@@ -0,0 +1,25 @@
+
+function myFun() {
+	var a = document.myForm.Email.value;     // raju@gmail.com
+	if(a.indexOf('@')<=0) {   //@gmail.com
+		document.getElementById("Message").innerHTML ="Invalid @ position ";
+		return false;
+	}
+	//thapa@gmail.com  
+	if(a.charAt(a.length-4)!='.') {
+		document.getElementById("Message").innerHTML ="Invalid . position ";
+		return false;
+	}
+	//thapa@gmail.co
+	if(a.charAt(a.length-3)!='.') {
+		document.getElementById("Message").innerHTML ="Invalid . position ";
+		return false;
+	}
+	
+
+}
+
+    
+    
+    
+    
